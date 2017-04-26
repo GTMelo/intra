f6dd05b64681e4c2d2253a2fa3b7335b12046823
Gustavo Ericson <gustavo.alexandre@fazenda.gov.br>
2017-04-26 16:58:15 -0300

    Experimenting with controller strategies

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function noticia(\App\Models\Objeto $objeto){
+        return view('noticia', compact('objeto'));
+    }
+    
+    public function pagina(\App\Models\Objeto $objeto){
+        return view('pagina', compact('objeto'));
+    }
 }

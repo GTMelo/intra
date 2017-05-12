049c0596fb200170325771dbbef55e50447acba7
Gustavo Ericson <gustavo.alexandre@fazenda.gov.br>
2017-05-12 17:43:24 -0300

    Tweaks to migrations and added cidades, estados and paises

---
+++ database/migrations/2017_05_12_094547_create_estados_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->integer('capital')->unsigned()->nullable();
+            $table->integer('pais_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estados');
+    }
+}

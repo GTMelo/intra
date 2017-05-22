c287c0cc275d78d270216cf2b2eccd55e4cf5463
Gustavo Ericson <gustavo.alexandre@fazenda.gov.br>
2017-05-22 12:48:10 -0300

    Created functions for dealing with Unidade and its Unidade_superior elements

--- app/Models/Unidade.php
+++ app/Models/Unidade.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Unidade extends Model
 {
-    //
+    public function unidade_superior(){
+
+        return $this->hasOne(self::class, 'id', 'unidade_superior_id');
+
+    }
+
+    public function hierarquia(){
+
+        $result = [];
+
+        $unidade_atual = $this->unidade_superior;
+
+        while($unidade_atual != null){
+
+            array_unshift($result, $unidade_atual);
+            $unidade_atual = $unidade_atual->unidade_superior;
+
+        }
+
+        return $result;
+
+    }
 }

0f5b1745f0df4c73b7590dc7eb1d92742abdffe7
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-05-30 00:30:15 -0300

    Moved date range function to helpers file and made more reusable. Removed lista_aniversariantes view (it was bugged anyways)

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 function wryyyy()
 {
     return 'Wryyyyyy!';
@@ -10,5 +12,30 @@ function createPath($path, $mode = 0755)
 
     if (is_dir($path)) return false;
     return mkdir($path, 0755, true);
+
+}
+
+function isDayMonthInRange(Carbon $dateToEval, $rangeStart, $rangeStop, Carbon $rangeBase = null)
+{
+
+    if (!isset($rangeBase)) $rangeBase = new Carbon();
+
+    $range = [
+        'start' => $rangeBase->copy()->addDays($rangeStart),
+        'end' => $rangeBase->copy()->addDays($rangeStop)
+    ];
+    $loop = $range['start'];
+    $date = $dateToEval;
+
+    while ($loop->diffInDays($range['end']) != 0) {
+
+        if ($date->isSameAs('md', $loop)) {
+            return true;
+        }
+        $loop->addDay();
+
+    }
+
+    return false;
 
 }

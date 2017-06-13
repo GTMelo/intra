e7fa4b6cf2f0efaef884acecf4fb84a14e6455cb
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-06-13 00:27:50 -0300

    Some late-night tweaks to scss

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -39,7 +39,7 @@
     <div id="header_search">
         <form method="get" id="header_search_form" action="/pesquisa">
             <input type="search" name="q"/>
-            <input type="submit" value=""/>
+            <input type="submit" value="Pesquisar"/> {{--TODO trocar pra ícone--}}
         </form>
     </div>
     <div id="header_user">

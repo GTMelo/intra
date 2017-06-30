4e29d34c544da20b0bacd7561cfae1b6e9e449ef
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-06-30 00:57:14 -0300

    Some work in the views.

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -11,16 +11,16 @@ class PermissionTableSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Permission::create([
-            'name' => 'Publicação de Artigo',
-            'slug' => 'can-publish-artigo',
-            'description' => 'Pode publicar artigos'
-        ]);
-
-        \App\Models\Permission::create([
-            'name' => 'Publicação de Artigos Global',
-            'slug' => 'can-publish-artigo-global',
-            'description' => 'Pode publicar artigos em qualquer unidade registrada'
-        ]);
+//        \App\Models\Permission::create([
+//            'name' => 'Publicação de Artigo',
+//            'slug' => 'can-publish-artigo',
+//            'description' => 'Pode publicar artigos'
+//        ]);
+//
+//        \App\Models\Permission::create([
+//            'name' => 'Publicação de Artigos Global',
+//            'slug' => 'can-publish-artigo-global',
+//            'description' => 'Pode publicar artigos em qualquer unidade registrada'
+//        ]);
     }
 }
--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -16,29 +16,29 @@ class RoleTableSeeder extends Seeder
         \App\Models\Role::truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        \App\Models\Role::create([
-            'name' => 'Owner Admin',
-            'slug' => 'admin-owner',
-            'description' => 'Admin principal do sistema'
-        ]);
-
-        \App\Models\Role::create([
-            'name' => 'Administrador',
-            'slug' => 'admin',
-            'description' => 'Admistrador do Sistema'
-        ]);
-
-        \App\Models\Role::create([
-            'name' => 'Usuário',
-            'slug' => 'user',
-            'description' => 'Usuário básico'
-        ]);
-
-        \App\Models\Role::create([
-            'name' => 'Usuário Não Confirmado',
-            'slug' => 'user-guest',
-            'description' => 'Usuário ainda não confirmado'
-        ]);
+//        \App\Models\Role::create([
+//            'name' => 'Owner',
+//            'slug' => 'admin-owner',
+//            'description' => 'Admin principal do sistema'
+//        ]);
+//
+//        \App\Models\Role::create([
+//            'name' => 'Administrador',
+//            'slug' => 'admin',
+//            'description' => 'Admistrador do Sistema'
+//        ]);
+//
+//        \App\Models\Role::create([
+//            'name' => 'Usuário',
+//            'slug' => 'user',
+//            'description' => 'Usuário básico'
+//        ]);
+//
+//        \App\Models\Role::create([
+//            'name' => 'Usuário Não Confirmado',
+//            'slug' => 'user-guest',
+//            'description' => 'Usuário ainda não confirmado'
+//        ]);
 
 
 

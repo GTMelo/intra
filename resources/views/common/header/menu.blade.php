06910d91ff512b7a7c155a116d5a4e0e859cdb68
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-08-03 17:27:04 -0300

    Reworking style one last time. This time elements will be styled based on objects.

--- resources/views/common/header/menu.blade.php
+++ resources/views/common/header/menu.blade.php
@@ -1,26 +1,36 @@
 <div id="header_menu">
     {{--<nav><a href="/">Página Principal</a></nav>--}}
-    <nav><a href="/legislacao">Legislação</a></nav>
-    <nav class="has-dropdown">
-        <span class="dropdown-button">Áreas da SAIN</span>
-        <nav class="dropdown-content dpd-smallj">
-            <ul>
-                <li><a href="/areas/sain">SAIN</a></li>
-                <li><a href="/areas/cgab">CGAB</a></li>
-                <li><a href="/areas/dilid">DILID</a></li>
-            </ul>
-        </nav>
+    <nav>
+        <div>
+            <a href="/legislacao">Legislação</a>
+        </div>
     </nav>
     <nav class="has-dropdown">
-        <span class="dropdown-button">SAIN na Internet</span>
-        <nav class="dropdown-content wider">
-            <ul>
-                <li><a href="http://sain.fazenda.gov.br">Sain.fazenda</a></li>
-                <li><a href="http://pcn.fazenda.gov.br">PCN.fazenda</a></li>
-                <li><a href="https://webmail.fazenda.gov.br">Exchange</a></li>
-                <li><a href="https://expresso.fazenda.gov.br">Expresso</a></li>
-            </ul>
-        </nav>
+        <div class="dropdown-trigger">
+            <span>Unidades</span>
+            <div class="dropdown-content">
+                <ul>
+                    <li><a href="/areas/sain">SAIN</a></li>
+                    <li><a href="/areas/cgab">CGAB</a></li>
+                    <li><a href="/areas/dilid">DILID</a></li>
+                </ul>
+            </div>
+        </div>
     </nav>
-    <nav class="has-dropdown">Ferramentas</nav>
+    <nav class="has-dropdown">
+        <div class="dropdown-trigger">
+            <span class="dropdown-trigger">SAIN na Internet</span>
+            <div class="dropdown-content">
+                <ul>
+                    <li><a href="/areas/sain">SAIN</a></li>
+                    <li><a href="/areas/cgab">PCN</a></li>
+                    <li><a href="/areas/cgab">Email Exchange</a></li>
+                    <li><a href="/areas/cgab">Email Expresso</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <nav class="has-dropdown">
+        <div>Ferramentas</div>
+    </nav>
 </div>
